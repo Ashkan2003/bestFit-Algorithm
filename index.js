302fe882184f8279b1c8d7dc6f07491e6a030b5a
ashkan <ashkanpc2003@gmail.com>
2024-01-01 10:47:49 +0330

    v-05

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ runBtn.addEventListener('click', function (event) {
     blockSizeInputValue = blockSizeInput.value
     processSizeInputValue = processSizeInput.value
 
-    // console.log(blockSizeInputValue, processSizeInputValue, "ssss")
 
     let blockSizeInputArray = blockSizeInputValue.split(',').map(function (item) {
         return parseInt(item);
@@ -29,24 +28,25 @@ runBtn.addEventListener('click', function (event) {
 
     let processSizeInputArrayLength = processSizeInputArray.length
 
+    console.log(blockSizeInputArray, blockSizeInputArrayLength, processSizeInputArray, processSizeInputArrayLength)
     bestFit(blockSizeInputArray, blockSizeInputArrayLength, processSizeInputArray, processSizeInputArrayLength)
 
 
 })
 
-function bestFit(blockSize, m, processSize, n) {
+function bestFit(blockArray, blockArrayLength, processArray, processArrayLength) {
     // Stores block id of the block allocated to a process 
-    let allocation = new Array(n).fill(-1);
-
+    let allocation = new Array(processArrayLength).fill(-1); // create a array of length of processArrayLength and fill its items with "-1" like [-1, -1, -1, -1]
+    
     // Pick each process and find suitable blocks according to its size and assign to it 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < processArrayLength; i++) {
         // Find the best fit block for current process 
         let bestIdx = -1;
-        for (let j = 0; j < m; j++) {
-            if (blockSize[j] >= processSize[i]) {
+        for (let j = 0; j < blockArrayLength; j++) {
+            if (blockArray[j] >= processArray[i]) {
                 if (bestIdx === -1) {
                     bestIdx = j;
-                } else if (blockSize[bestIdx] > blockSize[j]) {
+                } else if (blockArray[bestIdx] > blockArray[j]) {
                     bestIdx = j;
                 }
             }
@@ -56,32 +56,32 @@ function bestFit(blockSize, m, processSize, n) {
         if (bestIdx !== -1) {
             // Allocate block j to p[i] process 
             allocation[i] = bestIdx;
-
             // Reduce available memory in this block. 
-            blockSize[bestIdx] -= processSize[i];
+            blockArray[bestIdx] -= processArray[i];
         }
     }
-
-    console.log("Process No. Process Size     Block no.");
-    for (let i = 0; i < n; i++) {
-        console.log(`${i + 1}         ${processSize[i]}         ${allocation[i] !== -1 ? allocation[i] + 1 : "Not Allocated"}`);
+    console.log(allocation,"aaa")
+    console.log("Process No. Process Size  Block no. blockLeftOver");
+    for (let i = 0; i < processArrayLength; i++) {
+        console.log(`${i + 1}         ${processArray[i]}         ${allocation[i] !== -1 ? allocation[i] + 1 : "Not Allocated"}         ${blockArray[allocation[i]]}`);
     }
     // --------------
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < processArrayLength; i++) {
         tableContainer.insertAdjacentHTML("beforeend", `
         <tr>
         <td>${i + 1}</td>
-        <td>${processSize[i]}</td>
+        <td>${processArray[i]}</td>
         <td>${allocation[i] !== -1 ? allocation[i] + 1 : "Not Allocated"}</td>
+        <td>${blockArray[allocation[i]]}</td>
         </tr>
        `)
     }
 }
 
 // Driver code
-let blockSize = [100, 500, 200, 300, 600];
-let processSize = [212, 417, 112, 426];
-let m = blockSize.length;
-let n = processSize.length;
-bestFit(blockSize, m, processSize, n);
+let blockArray = [100, 500, 200, 300, 600];
+let processArray = [212, 417, 112, 426];
+let blockArrayLength = blockArray.length;
+let processArrayLength = processArray.length;
+bestFit(blockArray, blockArrayLength, processArray, processArrayLength);
 
